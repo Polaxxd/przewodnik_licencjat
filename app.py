032de479d86571836a4271fed803c615cff70522
Polaxxd <polapalonek@gmail.com>
2023-05-14 20:13:44 +0200

    all files

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.htm')
+
+@app.route('/kultura')
+def kultura():
+    return render_template('kultura.htm')
+
+@app.route('/daktylografia')
+def daktylografia():
+    return render_template('daktylografia.htm')
+
+@app.route('/daktylografia/teoria')
+def daktylografia_teoria():
+    return render_template('teoria.htm')
+
+@app.route('/o_serwisie')
+def o_serwisie():
+    return render_template('o_serwisie.htm')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+# ssh 20_palonek@limba.wzks.uj.edu.pl -p 4016
