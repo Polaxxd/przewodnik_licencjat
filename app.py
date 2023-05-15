db007457e79f8bbba8dc0c5e5318516238033ad1
Polaxxd <polapalonek@gmail.com>
2023-05-15 13:32:21 +0200

    404 error page created

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def daktylografia_teoria():
 def o_serwisie():
     return render_template('o_serwisie.htm')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.htm'), 404
+
+
 if __name__ == '__main__':
     app.run()
 
