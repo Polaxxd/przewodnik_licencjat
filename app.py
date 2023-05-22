6912a70184b05bc269da22c86c4344ce1adc0f9a
Polaxxd <polapalonek@gmail.com>
2023-05-22 10:19:31 +0200

    updating and deleting users in db; passwords added

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask import render_template
 from flask import flash
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "przewodnik_pola"
 #Add database
 db = SQLAlchemy()
+migrate = Migrate(app, db)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://20_palonek:polap1103@127.0.0.1/20_palonek'
 # Initialize The Database
 db.init_app(app)
@@ -23,17 +27,33 @@ class Users(db.Model):
     name = db.Column(db.String(200), nullable=False)
     nick = db.Column(db.String(120), nullable=False, unique=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # Passwords
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Hasło nie może być wyświetlone.')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Create A String
     def __repr__(self):
         return '<Name %r>' % self.name
 
-# Create a Form Class to add users
-class UserForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    nick = StringField("Nick", validators=[DataRequired()])
-    submit = SubmitField("Submit")
 
+################## FORMS #####################
+# Create a Form Class to add and update users
+class UserForm(FlaskForm):
+    name = StringField("Imię", validators=[DataRequired()])
+    nick = StringField("Nick", validators=[DataRequired()])
+    password_hash = PasswordField('Hasło', validators=[DataRequired(), EqualTo('password_hash2', message="Hasła muszą być takie same!")])
+    password_hash2 = PasswordField('Powtórz hasło', validators=[DataRequired()])
+    submit = SubmitField("Zapisz")
 
 # Create a Form Class
 class NamerForm(FlaskForm):
@@ -60,12 +80,12 @@ def daktylografia_teoria():
 def o_serwisie():
     return render_template('o_serwisie.htm')
 
+@app.route('/logowanie')
+def login():
+    return render_template('user.htm')
 
-# Create Name Page
 @app.route('/uzytkownik', methods=['GET', 'POST'])
 def name():
-    # Nname = None
-
     Nname = None
     Nform = NamerForm()
     # Validate Form
@@ -87,7 +107,9 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(nick=form.nick.data).first()
         if user is None:
-            user = Users(name=form.name.data, nick=form.nick.data)
+            #hash the password
+            hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+            user = Users(name=form.name.data, nick=form.nick.data, password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
             flash("Dodano użytkownika!")
@@ -97,42 +119,64 @@ def add_user():
         name = form.name.data
         form.name.data = ''
         form.nick.data = ''
+        form.password_hash.data = ''
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.htm",
         form=form,
         name=name,
         our_users=our_users)
 
-@app.route('/uzytkownik/<int:id>', methods=['GET', 'POST'])
+@app.route('/uzytkownik/edytuj/<int:id>', methods=['GET', 'POST'])
 def update(id):
     form = UserForm()
-    name_to_update = Users.query.get_or_404(id)
+    user_to_update = Users.query.get_or_404(id)
     edited = None
     if request.method == "POST":
         user = Users.query.filter_by(nick=form.nick.data).first()
-        if user == name_to_update.nick:
+        if user == user_to_update.nick:
             user=None
         if user is None:
-            name_to_update.name = request.form['name']
-            name_to_update.nick = request.form['nick']
+            user_to_update.name = request.form['name']
+            user_to_update.nick = request.form['nick']
             try:
                 db.session.commit()
                 flash("Zmieniono dane.")
                 edited = "tak"
-                return render_template("edytuj.htm",
-                                       form = form,
-                                       name_to_update = name_to_update)
+                title = "Zmieniono dane."
+                return render_template("back_to_users.htm", title = title)
             except:
                 flash("Wystąpił błąd, spróbuj ponownie.")
-                return render_template("edytuj.htm",
+                return render_template("update.htm",
                                        form=form,
-                                       name_to_update=name_to_update)
+                                       user_to_update=user_to_update)
         else:
+            # uwaga! to psuje, jeśli użytkownik nie zmieni nicku! coś jest źle!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! <-rozwiązanie: można usunąć możliwość zmiany nicku
             flash("Użytkownik o tym nicku już istnieje. Wybierz inną nazwę użytkownika.")
     else:
-        return render_template("edytuj.htm",
-                                form=form,
-                                name_to_update=name_to_update)
+        return render_template("update.htm",
+                               form=form,
+                               user_to_update=user_to_update,
+                               id=id)
+
+@app.route('/uzytkownik/usun/<int:id>')
+def delete(id):
+    name = None
+    deleted = None
+    title = "Usunięto użytkownika."
+    form = UserForm()
+    user_to_delete = Users.query.get_or_404(id)
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("Usunięto użytkownika")
+        deleted = "tak"
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("back_to_users.htm", title = title)
+    except:
+        flash("Wystąpił błąd, spróbuj ponownie.")
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("add_user.htm", form=form, name=name, our_users=our_users)
+
 
 
 @app.errorhandler(404)
